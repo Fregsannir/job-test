2b58d0cda75e325c8963f45bfb1101472e008648
Teimuraz Kozanashvili <kozanashvili.teimuraz@gmail.com>
2023-06-29 13:50:03 +0300

    Common module with global utils and types
    Added:
     - decorators submodule with transform decorators for string trim and case change;
     - EntityID type.

---
+++ src/common/decorators/trim.decorator.ts
@@ -0,0 +1,32 @@
+import {
+    Transform,
+    TransformFnParams,
+    TransformOptions,
+} from 'class-transformer';
+
+export type TrimOptions = 'start' | 'end';
+
+/**
+ * Removes whitespaces from a string by provided trim options.
+ *
+ * By default removes whitespaces from both ends of a string.
+ */
+export function Trim(
+  trimOptions?: TrimOptions,
+  options?: TransformOptions,
+): PropertyDecorator {
+  return Transform(({ key, value }: TransformFnParams) => {
+    if (typeof value !== 'string') {
+      throw new Error(`'${key}' must be a type of string.`);
+    }
+
+    switch (trimOptions) {
+      case 'start':
+        return value.trimStart();
+      case 'end':
+        return value.trimEnd();
+      default:
+        return value.trim();
+    }
+  }, options);
+}
