10b1edf4fc4c7d23c494c274071ef6f9cf339b12
Teimuraz Kozanashvili <kozanashvili.teimuraz@gmail.com>
2023-06-29 13:47:16 +0300

    Configure sequelize and config module, rewrite bootstrap in main.ts
    Added:
     - SequelizeModule and ConfigModule initialization;
     - application port loading into boostrap method from .env;
     - use global validation pipe with transform and whitelist.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { AppConfiguration, validate } from './app.configuration';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      cache: true,
+      isGlobal: true,
+      validate,
+    }),
+    SequelizeModule.forRootAsync({
+      inject: [ConfigService<AppConfiguration, true>],
+      useFactory: (configService: ConfigService<AppConfiguration, true>) => ({
+        dialect: 'postgres',
+        host: configService.get<string>('APP_DB_HOST'),
+        port: configService.get<number>('APP_DB_PORT'),
+        username: configService.get<string>('APP_DB_USERNAME'),
+        password: configService.get<string>('APP_DB_PASSWORD'),
+        database: configService.get<string>('APP_DB_NAME'),
+        models: [`${__dirname}/**/entities/*.ts`],
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { AppConfiguration } from 'app.configuration';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService: ConfigService<AppConfiguration, true> = app.get(
+    ConfigService<AppConfiguration, true>,
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+  await app.listen(configService.get<number>('APP_PORT'));
 }
 bootstrap();
